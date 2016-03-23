6a5125c8a3d963e160a9f2d2d20319f211505059
GirlWhoCodes1212 <almeidasmail@gmail.com>
2016-03-22 22:05:25 -0400

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,13 @@
+/* Stephanie Almeida
+    Example Program 
+    Shows basic elements of C++, will be edited as more and more lessons pass by
+    3/23/16 */
+
+#include <iostream>                                                         //Including necessary libraries and namespace std
+using namespace std;
+
+int main() {                                                                //main function
+
+  cout << "Hello World!" << endl;                                           //output statement
+
+return 0; }                                                                 //end of program
