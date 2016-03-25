378c58794963c68f20ccde420db5ff55fd77c950
GirlWhoCodes1212 <almeidasmail@gmail.com>
2016-03-24 23:05:28 -0400

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -8,6 +8,10 @@ using namespace std;
 
 int main() {                                                                //main function
 
+  int num1 = 5;                                                             //declaring an int variable
+  float dec1 = 0.5;                                                         //declaring a float variable
+
   cout << "Hello World!" << endl;                                           //output statement
+  cout << num1 * dec1 << endl;                                              //output 2.5
 
 return 0; }                                                                 //end of program
