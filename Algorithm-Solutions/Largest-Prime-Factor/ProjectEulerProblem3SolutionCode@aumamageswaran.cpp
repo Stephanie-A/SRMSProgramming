9ff36dde25e23c2bfc3edd073426cf0237491618
Akul Umamageswaran <akulum2002@gmail.com>
2016-05-05 21:00:26 -0400

    ProjectEulerProblem3SolutionCode.cpp
    @aumamageswaran

--- Algorithm-Solutions/Largest-Prime-Factor/ProjectEulerProblem3SolutionCode@aumamageswaran.cpp
+++ Algorithm-Solutions/Largest-Prime-Factor/ProjectEulerProblem3SolutionCode@aumamageswaran.cpp
@@ -7,12 +7,13 @@
 using namespace std;
 
 int main() {
-    long long int num;
-    int largest = 0;
+    long long num;
+    long long largest = 0;
     
     cin >> num;
+    int halfNum = num / 2;
     
-    for (int i = 2; i < num; i++) {
+    for (int i = 2; i <= halfNum; i++) {
         bool isPrime = true;
         if (num % i == 0) {
             for (int a = 2; a <= i-1; a++) {
