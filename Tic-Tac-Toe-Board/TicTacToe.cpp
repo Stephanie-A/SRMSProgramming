55eb3f90921c47b0a8e077c160dfadcca38f6135
GirlWhoCodes1212 <almeidasmail@gmail.com>
2016-06-01 19:34:16 -0400

    Create TicTacToe.cpp

---
+++ Tic-Tac-Toe-Board/TicTacToe.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void board(char x[3][3]);
+
+int main() {
+}
+
+void board(char x[3][3]) {
+	cout << "  0  1  2 " << "/n" << "0 " << x[0][0] << " | " << x[1][0] << " | " << x[2][0]
+}
