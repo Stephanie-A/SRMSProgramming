0019af81edff89c9c020fa291bdeaa7999abf996
Lauren <almeidalmail@gmail.com>
2017-02-15 07:33:28 -0500

    Update Calculator_lauren.cpp

--- Calculator_lauren.cpp
+++ Calculator_lauren.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std; 
-int main () {
+  #include <iostream>
+  using namespace std; 
+  int main () {
 
-float num1;
-float num2;
-cin>>num1;
-cin>>num2;
-cout<< "Your Choice:"; 
-cin>>choice
-if (choice==1) {
-result= num1-num2; 
+  float num1;
+  float num2;
+  cin>>num1;
+  cin>>num2;
+  cout<< "Your Choice:"; 
+  cin>>choice
+  if (choice==1) {
+  result= num1-num2; 
 
-else if (choice==2) }
-result=num1+num2;
+  else if (choice==2) }
+  result= num1+num2;
 
-cout<< result<<endl;
+  cout<< result<<endl;
 
   return 0;
 }
