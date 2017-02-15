56f1f0820575e6538b6a5ff4bfd681cf3c35b48f
Lauren <almeidalmail@gmail.com>
2017-02-15 07:30:53 -0500

    Update Calculator_lauren.cpp

--- Calculator_lauren.cpp
+++ Calculator_lauren.cpp
@@ -10,10 +10,10 @@ cout<< "Your Choice:";
 cin>>choice
 if (choice==1) {
 result= num1-num2; 
-}
-else if (choice==2) {
+
+else if (choice==2) }
 result=num1+num2;
-}
+
 cout<< result<<endl;
 
   return 0;
