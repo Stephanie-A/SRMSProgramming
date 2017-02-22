b25013a077cd0d37afc0d5c2b2e4d7f4cb458304
Lauren <almeidalmail@gmail.com>
2017-02-22 07:43:56 -0500

    Update and rename Calculator to Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int num1= 0;
+int dec1= 4;
+for (int i=0; i<4;i++){
+cout<<" Please enter num1 ";
+cin >> num1;
+cout<< " Please enter dec1 ";
+cin >> dec1;
+cout<< num1*dec1 << endl;
+cout<< num1+dec1 << endl;
+cout<< num1-dec1 <<endl;
+cout<< num1/dec1 <<endl;
+cout<< endl; }
+}
+
+
